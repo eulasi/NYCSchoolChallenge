34ed36875d42d6fc72f51dc41507a1986682e89a
Emmanuel <96310496+eulasi@users.noreply.github.com>
2024-04-02 14:18:26 -0400

    fix: theme updated

--- app/src/main/java/com/clone/schoolsapplicationtwo/ui/screens/SchoolListScreen.kt
+++ app/src/main/java/com/clone/schoolsapplicationtwo/ui/screens/SchoolListScreen.kt
@@ -2,6 +2,7 @@ package com.clone.schoolsapplicationtwo.ui.screens
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -16,7 +17,7 @@ import com.clone.schoolsapplicationtwo.ui.vm.SchoolsViewModel
 fun SchoolListScreen(viewModel: SchoolsViewModel) {
     val schools by viewModel.schools.observeAsState(emptyList())
 
-    LazyColumn {
+    LazyColumn(modifier = Modifier.padding(18.dp)) {
         items(schools) { school ->
             Text(text = "${school.schoolName} - ${school.dbn}")
             Text(text = "Overview: ${school.overviewParagraph ?: "N/A"}")
